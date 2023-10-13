36adc91866cac01c4dd0d8f6c59f7c47b5ca11ea
patcharin8 <139011526+patcharin8@users.noreply.github.com>
2023-10-14 03:16:53 +0700

    Add files via upload

--- herb/admin/report_date.php
+++ herb/admin/report_date.php
@@ -1,13 +1,4 @@
-<?php include 'condb.php';?>
-<?php
-$ids = $_GET['id'];
-//แสดงรูปหลักฐานการชำระเงิน
-$sql = "SELECT * FROM table_order t, payment m WHERE t.orderID=m.orderID AND t.orderID ='$ids'";
-$result=mysqli_query($conn,$sql);
-$row1=mysqli_fetch_array($result);
-$image_bill=$row1['pay_image'];
-
-?>
+<?php include 'condb.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,7 +8,7 @@ $image_bill=$row1['pay_image'];
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>report</title>
+    <title>รายงาน</title>
 
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
@@ -25,6 +16,7 @@ $image_bill=$row1['pay_image'];
 </head>
 
 <body class="sb-nav-fixed">
+    <br>
     <?php include 'menu_ad.php'; ?>
     <div id="layoutSidenav_content">
         <main>
@@ -33,67 +25,66 @@ $image_bill=$row1['pay_image'];
                 <div class="card mb-4">
                     <div class="card-header">
                         <i class="fas fa-table me-1"></i>
-                        แสดงรายการสินค้าการสั่งซื้อ
+                        แสดงรายงานข้อมูลการสั่งซื้อสินค้า 
                     </div>
                     <br>
-                    <div>
-                        <a href="report_order.php">
-                            <button type="button" class="btn btn-outline-success">กลับหน้าหลัก</button></a>
-
-                    </div>
+                   
                     <div class="card-body">
-                        <h6>เลขใบสั่งซื้อ : <?= $ids ?></h6>
-                        <table class="table table-striped">
+                        <table id="datatablesSimple" class="table table-striped">
                             <thead>
                                 <tr>
+                                    <th>เลขใบสั่งซื้อ</th>
                                     <th>รหัสสินค้า</th>
                                     <th>ชื่อสินค้า</th>
-                                    <th>ราคา</th>
-                                    <th>จำนวน</th>
+                                    <th>จำนวนสินค้า</th>
                                     <th>ราคารวม</th>
+                                    <th>วันที่สั่งซื้อ</th>
                                 </tr>
                             </thead>
-
-                            <?php
-                            $sql = "SELECT * FROM order_detail d, product p, table_order t where  d.orderID = t.orderID and d.pro_id = p.pro_id and d.orderID ='$ids'
-                            ORDER BY d.pro_id";
-                            $result = mysqli_query($conn, $sql);
-                            $sum_total =0;
-                            while ($row = mysqli_fetch_array($result)) {
-                                $sum_total=$row['total_price'];
-
-                            ?>
-
                                 <tbody>
+
+                                <?php
+                             $sql = "
+                             SELECT 
+                                 orderID ,
+                                 pro_id,
+                                 pro_name,
+                                 orderQty,
+                                 total_price,
+                                 reg_date,
+                                 product.pri_id,
+                                 d.reg_date
+                             FROM product AS p
+                             LEFT JOIN table_order ON p.orderID = table_order.pro_id
+                             LEFT JOIN order_detail AS d ON p.pro_id = d.orderID 
+                             WHERE p.reg_date = ' '
+                             ORDER BY orderID  DESC
+                             ";
+                            $result = mysqli_query($conn, $sql);
+                            while ($row = mysqli_fetch_array($result)) {
+                                
+                                
+                            ?>
                                     <tr>
+                                        <td><?= $row['orderID'] ?></td>
                                         <td><?= $row['pro_id'] ?></td>
                                         <td><?= $row['pro_name'] ?></td>
-                                        <td><?= $row['price'] ?></td>
                                         <td><?= $row['orderQty'] ?></td>
-                                        <td><?= $row['total'] ?></td>
-
+                                        <td><?= $row['total_price'] ?></td>
+                                        <td><?= $row['reg_date'] ?></td>
                                         <td>
-
+                                           
+                                        </td>
+                                        
                                     </tr>
-                                </tbody>
-                            <?php }
+                                    <?php }
                             mysqli_close($conn);
                             ?>
+                                </tbody>
+                            
                         </table>
-                        <b>ราคารวมสุทธิ <?=number_format($sum_total,2) ?> บาท</b>
                     </div>
                 </div>
-
-                <div class="text-center">
-                    <?php
-                    if($image_bill <> ""){?>
-                    <div class="text-center">
-                        <h5>ชำระเงินแล้ว</h5><br>
-                        <img src="<?=$image_bill?>" width="300">
-                    </div>
-                <?php  } else{ ?>
-                    <h5>ยังไม่ได้ชำระเงิน</h5>
-                <?php } ?>
             </div>
         </main>
         <?php include 'footer.php'; ?>
